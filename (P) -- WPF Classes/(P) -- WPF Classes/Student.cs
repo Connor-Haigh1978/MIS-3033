9db4944ff68f202f3ec60b078d3197171b680295
Connor Haigh <Connor.M.Haigh-1@ou.edu>
2021-02-25 13:55:46 -0600

    Inital Commit

---
+++ (P) -- WPF Classes/(P) -- WPF Classes/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P_____WPF_Classes
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public double GPA { get; set; } 
+        public Address Address { get; set; } 
+
+        Student()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Major = string.Empty;
+            GPA = -1;
+            Address = new Address();
+        }
+
+
+    }
+
+    public class Address
+    {
+        public int StreetNumber {get; set;}
+        public string StreetName { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public Address()
+        {
+            StreetNumber = 0;
+            StreetName = string.Empty;
+            State = string.Empty;
+            City = string.Empty;
+            ZipCode = 0;
+        }
+    }
+}
