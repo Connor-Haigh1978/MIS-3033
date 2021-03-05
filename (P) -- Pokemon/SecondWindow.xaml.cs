f8259a54518e21dd3067289db2bd32694058b6a9
Connor Haigh <Connor.M.Haigh-1@ou.edu>
2021-03-04 18:32:27 -0600

    Final Commit

--- (P) -- Pokemon/SecondWindow.xaml.cs
+++ (P) -- Pokemon/SecondWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,6 +19,8 @@ namespace _P_____Pokemon
     /// </summary>
     public partial class SecondWindow : Window
     {
+
+        IndividualPokemon api;
         public SecondWindow()
         {
             InitializeComponent();
@@ -24,9 +28,48 @@ namespace _P_____Pokemon
         }
         public void Image(ResultObject Pokemon)
         {
-            PokemonName.Text = Pokemon.ToString();
 
-            ImagePokemon.Source = new BitmapImage(new Uri(Pokemon.url));
+            //IndividualPokemon api;
+            
+            string url = $"{Pokemon.url}";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+
+                api = JsonConvert.DeserializeObject<IndividualPokemon>(json);
+
+            }
+
+
+            HeightBlockAnswer.Text = api.height;
+            WeightBlockAnswer.Text = api.weight;
+            NameBlock.Text = Pokemon.name;
+            SpriteImage.Source = new BitmapImage(new Uri(api.sprites.front_default));
+
+
+
+        }
+
+        private void ChangeSprite_Click(object sender, RoutedEventArgs e)
+        {
+            string status = ChangeSprite.Content.ToString().ToLower();
+            //IndividualPokemon api;
+            switch (status)
+            {
+                
+                case "change to back sprite":
+                    SpriteImage.Source = new BitmapImage(new Uri(api.sprites.back_default));
+                    ChangeSprite.Content = "change to front sprite";
+                    break;
+                case "change to front sprite":
+                    SpriteImage.Source = new BitmapImage(new Uri(api.sprites.front_default));
+                    ChangeSprite.Content = "change to back sprite";
+                    break;
+
+
+
+            }
 
         }
     }
