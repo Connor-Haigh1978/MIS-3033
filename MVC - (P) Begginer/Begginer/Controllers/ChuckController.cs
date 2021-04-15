9afac18edfc9d31c964bc1f08b8d0539deb4501b
Connor Haigh <connor.haigh1978@gmail.com>
2021-04-15 16:06:51 -0500

    INTIAL

---
+++ MVC - (P) Begginer/Begginer/Controllers/ChuckController.cs
@@ -0,0 +1,31 @@
+﻿using Begginer.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Begginer.Controllers
+{
+    public class ChuckController : Controller
+    {
+        // GET: Chuck
+        public ActionResult Index()
+        {
+
+            RandomJokeAPI joke;
+
+            using (var client = new HttpClient())
+            {
+                var JsonData = client.GetStringAsync("https://api.chucknorris.io/jokes/random").Result;
+
+                joke = JsonConvert.DeserializeObject<RandomJokeAPI>(JsonData);
+
+
+            }
+            return View(joke);
+        }
+    }
+}
